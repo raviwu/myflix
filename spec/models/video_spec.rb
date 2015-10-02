00063c8ae367538a334064ee784570c4ba8239ef
Ravi L.W. Wu <raviwu@gmail.com>
2015-10-02 09:59:11 +0800

    add video save itself test

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,6 +4,6 @@ describe Video do
   it "saves iteself" do
     video = Video.new(title: "New Video", description: "description of the new video", small_cover_url: "/url/s.jpg", large_cover_url:"/url/l.jpg")
     video.save
-    Video.last.title.should == "New Video"
+    expect(Video.last).to eq(video)
   end
 end
