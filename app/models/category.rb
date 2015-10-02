6c8c0ddd35780f6c0f9886b9f5f0b8c1bf1f495c
Ravi L.W. Wu <raviwu@gmail.com>
2015-10-03 06:32:11 +0800

    add test conditions

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,13 +1,13 @@
 class Category < ActiveRecord::Base
   has_many :video_classifications
-  has_many :videos, -> { order('title') }, through: :video_classifications
+  has_many :videos, -> { order('created_at DESC') }, through: :video_classifications
 
   validates :title, presence: true
 
   RECENT_VIDEO_QTY = 6
 
   def recent_videos
-    self.videos.sort_by {|v| v.created_at}.reverse.take(RECENT_VIDEO_QTY)
+    videos.limit(RECENT_VIDEO_QTY)
   end
 
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -5,6 +5,15 @@ describe Category do
   it { should validate_presence_of :title}
 
   describe "#recent_videos" do
+    it "returns videos in created_at desc order" do
+      comedy = Category.create(title: 'comedy')
+      up = Video.create(title: 'Up', description: 'description')
+      up.categories << comedy
+      se7en = Video.create(title: 'Se7en', description: 'description', created_at: 1.day.ago)
+      se7en.categories << comedy
+      expect(comedy.recent_videos).to eq([up, se7en])
+    end
+
     it "returns empty array if the category has no video" do
       comedy = Category.create(title: 'comedy')
       expect(comedy.recent_videos).to eq([])
@@ -14,28 +23,27 @@ describe Category do
       comedy = Category.create(title: 'comedy')
       up = Video.create(title: 'Up', description: 'description')
       up.categories << comedy
-      expect(comedy.recent_videos).to eq([up])
+      expect(comedy.recent_videos.count).to eq(1)
     end
 
     it "returns array contains only 6 videos if the category has more than RECENT_VIDEO_QTY video" do
       comedy = Category.create(title: 'comedy')
       videos = []
       over_insert = 2
-      (Category::RECENT_VIDEO_QTY + over_insert).times do |n|
-        videos[n] = Video.create(title: "video #{n}", description: 'description')
-      end
+      (Category::RECENT_VIDEO_QTY + over_insert).times {|n| videos[n] = Video.create(title: "video #{n}", description: 'description')}
       videos.each {|video| video.categories << comedy }
       videos.shift(over_insert)
       expect(comedy.recent_videos).to eq(videos.reverse)
     end
 
-    it "returns videos in created_at desc order" do
+    it "returns the most recent RECENT_VIDEO_QTY videos" do
       comedy = Category.create(title: 'comedy')
-      up = Video.create(title: 'Up', description: 'description')
-      up.categories << comedy
-      se7en = Video.create(title: 'Se7en', description: 'description', created_at: 1.day.ago)
-      se7en.categories << comedy
-      expect(comedy.recent_videos).to eq([up, se7en])
+      videos = []
+      Category::RECENT_VIDEO_QTY.times {|n| videos[n] = Video.create(title: "video #{n}", description: 'description')}
+      videos.each {|v| v.categories << comedy}
+      last_night_show = Video.create(title: "last night show", description: "last night show", created_at: 1.day.ago)
+      last_night_show.categories << comedy
+      expect(comedy.recent_videos).not_to include(last_night_show)
     end
 
     it "returns videos only belongs to the query category" do
