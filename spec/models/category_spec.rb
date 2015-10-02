f58749f3f2d50f6fd5589ee67049e64438a8ab93
Ravi L.W. Wu <raviwu@gmail.com>
2015-10-02 12:17:28 +0800

    refactore shoulda matchers for has_many relationship

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,20 +1,6 @@
 require 'spec_helper'
 
 describe Category do
-  it "save itself" do
-    category = Category.new(title: 'animation')
-    category.save
-    expect(Category.last).to eq(category)
-  end
-
-  it "has many videos" do
-    category = Category.create(title: 'fantacy')
-    south_park = Video.create(title: 'South Park', description: 'funny movie')
-    back_to_future = Video.create(title: 'Back to Future', description: 'time travel movie must see')
-    videos = Video.all
-    category.videos << videos
-    expect(category.videos).to eq([back_to_future, south_park])
-  end
-
+  it { should have_many :videos }
   it { should validate_presence_of :title}
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,21 +1,7 @@
 require 'spec_helper'
 
 describe Video do
-  it "saves iteself" do
-    video = Video.new(title: "New Video", description: "description of the new video", small_cover_url: "/url/s.jpg", large_cover_url:"/url/l.jpg")
-    video.save
-    expect(Video.last).to eq(video)
-  end
-
-  it "has many categories" do
-    south_park = Video.create(title: "South Park", description: "funny movie")
-    comedy = Category.create(title: 'comedy')
-    animation = Category.create(title: 'animation')
-    south_park.categories << Category.all
-    expect(south_park.categories).to eq([animation, comedy])
-  end
-
+  it { should have_many :categories }
   it { should validate_presence_of :title}
-
   it { should validate_presence_of :description}
 end
