9208b571fbc4c1cbf436ce2e15b929156553a01d
Ravi L.W. Wu <raviwu@gmail.com>
2015-10-03 15:19:23 +0800

    add categories_controller_spec tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -2,12 +2,29 @@ require 'spec_helper'
 
 describe CategoriesController do
   describe "GET index" do
-    it "sets the @categories variable"
-    it "redirects to log_in_path when not logged in"
+    it "redirects to sign_in_path when not logged in" do
+      get :index
+      response.should redirect_to(sign_in_path)
+    end
+    it "sets the @categories variable" do
+      user = User.create(email: "user@example.com", fullname: "user", password: "password")
+      session[:user_id] = user.id
+      get :index
+      assigns(:categories).should eq(Category.all)
+    end
   end
 
   describe "GET show" do
-    it "sets the @category"
-    it "redirects to log_in_path when not logged in"
+    it "redirects to sign_in_path when not logged in" do
+      get :show, id: 1
+      response.should redirect_to(sign_in_path)
+    end
+    it "sets the @category" do
+      user = User.create(email: "user@example.com", fullname: "user", password: "password")
+      session[:user_id] = user.id
+      comedy = Category.create(title: 'comedy')
+      get :show, id: comedy.id
+      assigns(:category).should eq(comedy)
+    end
   end
 end
